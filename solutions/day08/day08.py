0b4bec9114b2bc61f666ddd7cf7b002bd6da360d
Danielle Cohen <fonziepants@gmail.com>
2020-12-08 13:02:40 -0500

    Cleaned up Day08 - Remembered that part 1 checks for loops! - Thus, removed counter and added back "instruction-read" check - Updated accumulator method to print in any termination condition - Condensed code into single execute-program method - Removed term-condition parameters
    Now, it has less code, less complexity, and runs more quickly.

--- solutions/day08/day08.py
+++ solutions/day08/day08.py
@@ -28,55 +28,6 @@ def parse_int(string_with_int):
         multiplier *= 10
     return number
 
-def get_highest_instruction_count(instructions_read_map):
-    highest_read_count = 0
-    for key in instructions_read_map:
-        if instructions_read_map[key] > highest_read_count:
-            highest_read_count = instructions_read_map[key]
-    return highest_read_count
-
-def get_accumulator_value(instructions, terminate_on_last_line):
-    instructions_read_map = {}
-    accumulator = 0
-    index = 0
-
-    for instruction in instructions:
-        instructions_read_map[index] = 0
-        index += 1
-    
-    # reset index
-    index = 0
-
-    while (True):
-        # Part 1: check to see if we're about to re-read an instruction
-        if not terminate_on_last_line and instructions_read_map[index] == 1:
-            print("PROGRAM TERMINATING: instruction [{0}] was already read.".format(instructions[index]))
-            break
-
-        # read instruction
-        instructions_read_map[index] = instructions_read_map[index] + 1
-        instruction = instructions[index]
-        instruction_segments = instruction.split()
-        command = instruction_segments[0]
-        value = parse_int(instruction_segments[1])
-
-        # execute instruction
-        if command == "nop":
-            index += 1
-        elif command == "acc":
-            accumulator += value
-            index += 1
-        elif command == "jmp":
-            index += value
-        
-        # Part 2: check to see if we're about to read past the end of file
-        if terminate_on_last_line and index >= len(instructions):
-            print("PROGRAM TERMINATING: program has read to the end of file.")
-            break
-
-    print("Final accumulator value: {0}".format(accumulator))
-    return accumulator
-
 def create_all_instruction_versions(instructions):
     instruction_variants = []
 
@@ -95,19 +46,26 @@ def create_all_instruction_versions(instructions):
     
     return instruction_variants
 
-def program_terminates_at_end_of_file(instructions, max_counter=20000):
+def get_accumulator(instructions):
+    instructions_read_map = {}
     accumulator = 0
     index = 0
-    counter = 0
+
+    for instruction in instructions:
+        instructions_read_map[index] = 0
+        index += 1
+    
+    # reset index
+    index = 0
 
     while (True):
-        # check to see if we've run this program too long
-        if counter > max_counter:
-            print("PROGRAM TERMINATING: {0} instructions have been executed.".format(counter))
+        # check to see if we're about to re-read an instruction
+        if instructions_read_map[index] == 1:
+            print("PROGRAM TERMINATING: instruction [{0}] was already read.  Final accumulator value: {1}".format(instructions[index], accumulator))
             break
 
         # read instruction
-        counter += 1
+        instructions_read_map[index] = instructions_read_map[index] + 1
         instruction = instructions[index]
         instruction_segments = instruction.split()
         command = instruction_segments[0]
@@ -127,10 +85,10 @@ def program_terminates_at_end_of_file(instructions, max_counter=20000):
         
         # check to see if we're about to read past the end of file
         if index >= len(instructions):
-            print("PROGRAM TERMINATING: program has read to the end of file.")
-            return True
+            print("PROGRAM TERMINATING: program has read to the end of file. Final accumulator value: {0}".format(accumulator))
+            return accumulator
 
-    return False
+    return None
 
 def run_correct_instructions(original_instructions):
     # get the list of possible instruction variations
@@ -139,13 +97,10 @@ def run_correct_instructions(original_instructions):
     # run each of the instruction variations to find the correct one
     new_instructions = None
     for instructions in instruction_variants:
-        if program_terminates_at_end_of_file(instructions):
-            new_instructions = instructions
-
-    # get the accumulator for the correct instruction set
-    return get_accumulator_value(new_instructions,True)
+        accumulator = get_accumulator(instructions)
+        if accumulator:
+            return accumulator
 
 instructions = read_instructions("solutions\day08\day08_real.txt")
-# get_accumulator_value(instructions,False)
+get_accumulator(instructions)
 run_correct_instructions(instructions)
-
