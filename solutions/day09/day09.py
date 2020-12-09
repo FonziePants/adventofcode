4487f034dad95b9f5a289022bea6f7f3851de8ff
Danielle Cohen <fonziepants@gmail.com>
2020-12-09 12:48:55 -0500

    Day 9: Encoding Error, part 2 - Brute forced it again, LOL - Method iterates through all the possible sum ranges - Takes time to run but AOC incentivizes speed over performance so 🤷‍♀️

--- solutions/day09/day09.py
+++ solutions/day09/day09.py
@@ -14,7 +14,7 @@ def read_num_array(file_path):
 
     return num_array
 
-def find_first_invalid_number(full_num_array, preamble):
+def find_first_invalid_number(full_num_array, preamble, debug=False):
     if len(full_num_array) < preamble:
         return None
     
@@ -27,7 +27,8 @@ def find_first_invalid_number(full_num_array, preamble):
                 prev1 = full_num_array[j]
                 prev2 = full_num_array[k]
 
-                print("Checking to see if {0} is the sum of {1} and {2}".format(num,prev1,prev2))
+                if debug:
+                    print("Checking to see if {0} is the sum of {1} and {2}".format(num,prev1,prev2))
 
                 if num == (prev1 + prev2):
                     valid = True
@@ -37,12 +38,46 @@ def find_first_invalid_number(full_num_array, preamble):
         
         if not valid:
             print("Number {0} at index {1} is NOT valid. Stop processing.".format(num, i))
-            return num
+            return i #index of num
 
-        print("Number {0} at index {1} is valid. Proceed to next entry.".format(num, i))
+        if debug:
+            print("Number {0} at index {1} is valid. Proceed to next entry.".format(num, i))
+
+def find_contiguous_set_of_addends(full_num_array,index_of_num):
+    num_array_subset = full_num_array[:index_of_num]
+    sum_target = full_num_array[index_of_num]
+
+    # iterate through all the possible starting numbers
+    for start in range(len(num_array_subset)):
+
+        # iterate through all the possible sequence lengths
+        for stop in range(start+1,len(num_array_subset)):
+
+            sum = 0
+            highest_num = 0
+            lowest_num = sum_target * 10000
+            for i in range(start,stop+1): #stop+1 makes it inclusive
+                num = num_array_subset[i]
+                sum += num
+
+                if num > highest_num:
+                    highest_num = num
+                
+                if num < lowest_num:
+                    lowest_num = num
+            
+            print("Sum of values between index {0} (value = {1}) and index {2} (value = {3}) is {4}.".format(start,num_array_subset[start],stop,num_array_subset[stop],sum))
+            if sum == sum_target:
+                encryption_weakness = highest_num + lowest_num
+                print("Sequence found! Terminate program. Encryption weakness = {0}".format(encryption_weakness))
+                return encryption_weakness
+    
+    return -1
 
 # num_array = read_num_array("solutions\day09\day09_test.txt")
-# find_first_invalid_number(num_array,5)
+# index_of_invalid_num = find_first_invalid_number(num_array,5)
+# find_contiguous_set_of_addends(num_array, index_of_invalid_num)
 
 num_array = read_num_array("solutions\day09\day09_real.txt")
-find_first_invalid_number(num_array,25)
+index_of_invalid_num = find_first_invalid_number(num_array,25)
+find_contiguous_set_of_addends(num_array, index_of_invalid_num)
