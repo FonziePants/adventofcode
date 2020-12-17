baaa90c385a7ba15e128d9b498682279ae709946
Danielle Cohen <fonziepants@gmail.com>
2020-12-16 19:00:21 -0500

    Day 16 Part 2

--- solutions/day16/day16.py
+++ solutions/day16/day16.py
@@ -46,10 +46,22 @@ def read_data(file_path,debug=True):
 
     return (fields, tickets)
 
-def calculate_part1(data,debug=False):   
+def print_valid_values(valid_values):
+    print("VALID VALUES:")
+    for value in valid_values:
+        print("{0}: {1}".format(value, valid_values[value]))
+
+def remove_field(valid_values, field):
+    for value in valid_values:
+        if field in valid_values[value]:
+            valid_values[value].remove(field)
+    return valid_values
+
+def discard_invalid_tickets(data,debug=False):   
     fields = data[0]
     tickets = data[1]
     valid_values = {}
+    valid_tickets = []
     
     for field in fields:
         for value in field.values:
@@ -59,24 +71,63 @@ def calculate_part1(data,debug=False):
                 valid_values[value] = [field.name]
     
     if debug:
-        print("VALID VALUES:")
-        for value in valid_values:
-            print("{0}: {1}".format(value, valid_values[value]))
+        print_valid_values(valid_values)
     
     error_rate = 0
     for ticket in tickets:
+        valid = True
         for value in ticket:
             if value not in valid_values:
                 if debug:
                     print("{0} not found".format(value))
                 error_rate += value
+                valid = False
+        if valid:
+            valid_tickets.append(ticket)
     
     print("Part 1: {0}\n\n".format(error_rate))
-    return error_rate
+    return (fields,valid_tickets,valid_values)
 
 def calculate_part2(data,debug=False):
+    fields = data[0]
+    tickets = data[1]
+    valid_values = data[2]
+    field_indices = []
+    
+    for i in range(len(fields)):
+        field_indices.append(False)
 
-    print("Part 2: {0}\n\n".format("TODO"))
+    found_field = True
+    while found_field == True and False in field_indices:
+        found_field = False
+        # go through each field position
+        for i in range(len(field_indices)):
+            # get that field position's value for each ticket
+            values = []
+            for ticket in tickets:
+                if ticket[i] not in values:
+                    values.append(ticket[i])
+            # list out possible fields based on the value
+            possible_fields = valid_values[values[0]].copy()
+            # rule out fields by looking at all the other tickets
+            for j in range(1,len(values)):
+                for field in possible_fields:
+                    if field not in valid_values[values[j]]:
+                        possible_fields.remove(field)
+            if len(possible_fields) == 1:
+                field = possible_fields[0]
+                field_indices[i] = field
+                remove_field(valid_values,field)
+                found_field = True
+
+    print(field_indices)
+
+    answer = 1
+    for i in range(len(field_indices)):
+        if "departure" in field_indices[i]:
+            answer *= tickets[0][i]
+
+    print("Part 2: {0}\n\n".format(answer))
     return 
 
 def run_program(test=False, debug=False):
@@ -86,8 +137,11 @@ def run_program(test=False, debug=False):
     
     data = read_data(file_path, debug)
 
-    calculate_part1(data, debug)
-    calculate_part2(data, debug)
+    # PART 1
+    data2 = discard_invalid_tickets(data, debug)
+
+    # PART 2
+    calculate_part2(data2, debug)
 
 # run_program(True, True)
-run_program()
+run_program(False, False)
