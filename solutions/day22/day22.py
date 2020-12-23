d9e7efcb939063d1ae85f907ac11f1c2a5f8e6e4
Danielle Cohen <fonziepants@gmail.com>
2020-12-22 21:54:38 -0500

    Day 22 Part 2

--- solutions/day22/day22.py
+++ solutions/day22/day22.py
@@ -39,6 +39,9 @@ class Player:
     def has_cards(self):
         return len(self.cards) > 0
     
+    def count_cards(self):
+        return len(self.cards)
+    
     def play_card(self):
         if len(self.cards) == 0:
             return None
@@ -56,14 +59,75 @@ class Player:
         for card in cards:
             self.cards.append(card)
     
+    def get_hand_state(self):
+        return self.name.replace(" ","") + ":" + ",".join(list(map(str,self.cards)))
+    
+    def copy(self):
+        return Player(self.name, self.cards.copy())
+    
+    def copy_and_trim(self, size):
+        cards = self.cards[0:size]
+        return Player(self.name,cards)
+    
     def print(self):
-        print("{0}: {1}\n".format(self.name,self.cards))
+        print("{0}: {1}".format(self.name,self.cards))
 
+def play_game(player1, player2, round_outcomes, debug=False):
+    past_rounds = []
+    winner = None
 
-def calculate_part1(data,debug=False):   
-    player1 = data[0]
-    player2 = data[1]
+    if debug:
+        print("NEW GAME")
 
+    starting_round = player1.get_hand_state() + "|" + player2.get_hand_state()
+
+    # check for cache hits
+    if starting_round in round_outcomes:
+        return round_outcomes[starting_round]
+
+    # a "round"
+    while player1.has_cards() and player2.has_cards():
+        # check for infinite loop
+        round = player1.get_hand_state() + "|" + player2.get_hand_state()
+        if round in past_rounds:
+            round_outcomes[starting_round] = player1
+            return player1
+        else:
+            past_rounds.append(round)
+
+        p1 = player1.play_card()
+        p2 = player2.play_card()
+
+        if debug:
+            print("ROUND:\nPlayer 1: {0}\nPlayer 2: {1}\n".format(p1,p2))
+
+        # check for recursive combat
+        if player1.count_cards() >= p1 and player2.count_cards() >= p2:
+            winner = play_game(player1.copy_and_trim(p1), player2.copy_and_trim(p2), round_outcomes, debug)
+            if winner.name == player1.name:
+                player1.add_cards([p1, p2])
+            else:
+                player2.add_cards([p2, p1])
+        # otherwise, play normally
+        elif p1 > p2:
+            player1.add_cards([p1, p2])
+        else:
+            player2.add_cards([p2, p1])
+        
+        if debug:
+            print("GAME CONCLUSION\n")
+            player1.print()
+            player2.print()
+            print()
+
+    winner = player1 if player1.has_cards() else player2
+
+    # add outcome to cache to save time later
+    round_outcomes[starting_round] = winner
+
+    return winner
+
+def calculate_part1(player1, player2, debug=False):   
     while player1.has_cards() and player2.has_cards():
         p1 = player1.play_card()
         p2 = player2.play_card()
@@ -82,9 +146,10 @@ def calculate_part1(data,debug=False):
     print("Part 1: {0}\n\n".format(winner.count_score()))
     return
 
-def calculate_part2(data,debug=False):
+def calculate_part2(player1, player2, debug=False):
+    winner = play_game(player1, player2, {}, debug)
 
-    print("Part 2: {0}\n\n".format("TODO"))
+    print("Part 2: {0}\n\n".format(winner.count_score()))
     return 
 
 def run_program(test=False, debug=False):
@@ -93,9 +158,10 @@ def run_program(test=False, debug=False):
         file_path = "solutions\day22\day22_test.txt"
     
     data = read_data(file_path, debug)
-
-    calculate_part1(data, debug)
-    calculate_part2(data, debug)
+    player1 = data[0]
+    player2 = data[1]
+    calculate_part1(player1.copy(), player2.copy(), debug)
+    calculate_part2(player1.copy(), player2.copy(), debug)
 
 # run_program(True, True)
 run_program()
